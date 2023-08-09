983752664d8be72f8537328a9a41c6fea2cfe453
Nelis Drost <nelis.drost@gmail.com>
2023-08-09 13:36:17 +1200

    Implement cleaning/trimming of all vessel data

--- data_processing/load_data.py
+++ data_processing/load_data.py
@@ -23,6 +23,36 @@ def load_whales(file_name, bounds, crs):
     return wgdf
 
 
+def _clean_vessel_data(file_name):
+    """Procedure for cleaning and simplifying the input file 'data/vessels/AIS_3_29_filtered.gpkg'"""
+    gdf = gpd.read_file(file_name)
+
+    # Drop unwanted columns and rename for clarity
+    initial_columns = ['AIS_timest', 'Vessel_nam', 'Call_sign',
+                       # 'Length__me', 'Width__met', 'Orientatio', 'Speed__kno',  # May be interesting later
+                       'new_type', 'geometry']
+    rename_columns = {'AIS_timest': 'timestamp',
+                      # 'Length__me': 'length', 'Width__met': 'width',
+                      # 'Orientation': 'heading', 'Speed__kno': 'speed',  # May be interesting later
+                      'Vessel_nam': 'name', 'Call_sign': 'callsign', 'new_type': 'type'}
+    gdf = gdf[initial_columns].rename(columns=rename_columns)
+
+    # Some ships don't have call-signs, fill these in with truncated names
+    gdf.loc[gdf['callsign'].isna(), 'callsign'] = gdf.loc[gdf['callsign'].isna(), 'name'].str[:7]
+    gdf.loc[gdf['callsign'] == ' ', 'callsign'] = gdf.loc[gdf['callsign'] == ' ', 'name'].str[:7]
+    gdf.drop(columns=['name'], inplace=True)
+
+    # Sort by callsign and timestamp
+    gdf = (
+        gdf.sort_values(['callsign', 'timestamp'])
+        .reset_index(drop=True)
+    )
+
+    # Split by type and save the result
+    for vessel_type, group in gdf.groupby('type'):
+        group.reset_index(drop=True).to_file(f'data/vessels/{vessel_type}_points.gpkg', driver='GPKG')
+
+
 def load_vessels(file_name, crs):
     vessels = gpd.read_file(file_name)
 
