ff22e526207bea2382f4b3029dbea663655cc8df
Nelis Drost <nelis.drost@gmail.com>
2023-09-06 16:55:52 +1200

    Implement conversion of vessel points data to line segments

---
+++ plotting/util.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from shapely import LineString
+from tqdm import tqdm
+
+
+def points_to_segments(gdf):
+    """Converts a GeoDataFrame containing points to one where each row is a line segment between two points"""
+    # Sort by timestamp
+    gdf = gdf.sort_values('timestamp')
+
+    results = []
+    for callsign, group in tqdm(desc='Converting vessel points to segments', iterable=gdf.groupby('callsign')):
+        res = group.iloc[:-1].copy()
+        res['geometry'] = list(map(LineString, zip(group['geometry'][:-1],
+                                                   group['geometry'].shift(-1)[:-1])))
+        results.append(res)
+
+    out = pd.concat(results, ignore_index=True)
+
+    return out
