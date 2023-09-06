82d0a0de907cb0b60d6cd8d100b1f56b9ae5e247
Nelis Drost <nelis.drost@gmail.com>
2023-09-06 15:43:44 +1200

    Consistent naming of figure variable, tweak plotting

--- plotting/heatmap.py
+++ plotting/heatmap.py
@@ -15,23 +15,25 @@ def plot_traces(whale_df, vessel_df, protected_areas, basemap, bounds):
     colors = [Viridis256[i] for i in inds]
     cmapper = CategoricalColorMapper(factors=whale_names, palette=colors)
 
-    p = figure(width=1200, height=1200)
+    fig = figure(width=1200, height=1200)
 
-    p.patches('xs', 'ys', source=protected_source, fill_color='lightblue', line_alpha=1, fill_alpha=0.5)
+    fig.patches('xs', 'ys', source=protected_source, fill_color='lightblue', line_alpha=1, fill_alpha=0.5)
 
-    p.patches('xs', 'ys', source=basemap_source, fill_color='lightgreen', line_alpha=1, fill_alpha=0.5)
+    fig.patches('xs', 'ys', source=basemap_source, fill_color='lightgreen', line_alpha=1, fill_alpha=0.5)
 
-    p.multi_line('xs', 'ys', source=vessel_source, color='gray', line_width=1, line_alpha=0.1)
+    fig.multi_line('xs', 'ys', source=vessel_source, color='gray', line_width=1, line_alpha=0.1)
 
-    p.scatter('x', 'y', source=whale_source, color={'field': 'name', 'transform': cmapper}, fill_alpha=0.5)
+    fig.scatter('x', 'y', source=whale_source, color={'field': 'name', 'transform': cmapper}, fill_alpha=0.5)
+
+
 
     # Zoom to bounds
-    p.x_range.start = bounds[0]
-    p.x_range.end = bounds[2]
-    p.y_range.start = bounds[1]
-    p.y_range.end = bounds[3]
+    fig.x_range.start = bounds[0]
+    fig.x_range.end = bounds[2]
+    fig.y_range.start = bounds[1]
+    fig.y_range.end = bounds[3]
 
-    return p
+    return fig
 
 
 def plot_encounters(vessel_points, fig, max_dist=20000):
@@ -43,6 +45,6 @@ def plot_encounters(vessel_points, fig, max_dist=20000):
     cmap = LinearColorMapper(Inferno256, low=vessel_data['whale_dist'].max(), high=0)
 
     fig.scatter('x', 'y', source=vessel_source, color={'field': 'whale_dist', 'transform': cmap},
-                fill_alpha=1, size=20)
+                fill_alpha=0.2, size=10, line_color=None)
 
     return fig
