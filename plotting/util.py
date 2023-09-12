7a0ef980204bf166409b321380624ff57781b5c1
Nelis Drost <nelis.drost@gmail.com>
2023-09-12 17:59:08 +1200

    Generalise points_to_segments

--- plotting/util.py
+++ plotting/util.py
@@ -3,13 +3,13 @@ from shapely import LineString
 from tqdm import tqdm
 
 
-def points_to_segments(gdf):
+def points_to_segments(gdf, grouper):
     """Converts a GeoDataFrame containing points to one where each row is a line segment between two points"""
     # Sort by timestamp
     gdf = gdf.sort_values('timestamp')
 
     results = []
-    for callsign, group in tqdm(desc='Converting vessel points to segments', iterable=gdf.groupby('callsign')):
+    for _, group in tqdm(desc='Converting vessel points to segments', iterable=gdf.groupby(grouper)):
         res = group.iloc[:-1].copy()
         res['geometry'] = list(map(LineString, zip(group['geometry'][:-1],
                                                    group['geometry'].shift(-1)[:-1])))
@@ -18,6 +18,6 @@ def points_to_segments(gdf):
     out = pd.concat(results, ignore_index=True)
 
     # Convert timestamp to seconds since epoch (for easier comparison later)
-    out['timestamp'] = out['timestamp'].astype(int) // 10 ** 9
+    out['timestamp'] = out['timestamp'].astype('int64') // 10 ** 9
 
     return out
