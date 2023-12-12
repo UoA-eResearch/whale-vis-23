bce11addd333dd0555759513ee4b889616c71fd6
Nelis Drost <nelis.drost@gmail.com>
2023-12-13 12:05:25 +1300

    Set up generation of frames for different areas + time ranges

--- gen_anim_frames.py
+++ gen_anim_frames.py
@@ -7,8 +7,8 @@ from bokeh.io import export_png
 
 from data_processing import load_data
 from plotting import heatmap
-from plotting.heatmap import zoom_to_bounds
 from plotting.util import fix_dateline
+
 
 if __name__ == '__main__':
     # Load base data
@@ -24,12 +24,26 @@ if __name__ == '__main__':
     whales_interp = gpd.read_file('data/whales/whales_coast.gpkg')
 
     # Set up timestamps
-    start = '2022-08-01'
-    end = '2022-09-01'
-    timestamps = pd.date_range(start, end, freq='30min')
+    ranges = [('2020-07-01', '2023-06-01'),
+              ('2020-07-01', '2021-06-01'),
+              ('2021-07-01', '2022-06-01'),
+              ('2022-07-01', '2023-06-01')]
+    range_labels = ['full', '2020', '2021', '2022']
 
-    whale_mask = (whales_interp.timestamp >= start) & (whales_interp.timestamp < end)
-    vessel_mask = (vessel_points.timestamp >= start) & (vessel_points.timestamp < end)
+    timestamps = {
+        label: pd.date_range(start, end, freq='30min')
+        for label, (start, end) in zip(range_labels, ranges)
+    }
+
+    whale_mask = {
+        label: (whales_interp.timestamp >= start) & (whales_interp.timestamp < end)
+        for label, (start, end) in zip(range_labels, ranges)
+    }
+
+    vessel_mask = {
+        label: (vessel_points.timestamp >= start) & (vessel_points.timestamp < end)
+        for label, (start, end) in zip(range_labels, ranges)
+    }
 
     # Convert all gdfs to lat/long
     whales_interp = whales_interp.to_crs(4326)
@@ -37,9 +51,9 @@ if __name__ == '__main__':
     protected_areas = protected_areas.to_crs(4326)
     basemap = basemap.to_crs(4326)
     bounds_full = vessel_points.geometry.total_bounds
-    bounds_ant = [177.5, -51, 180, -47]
-    bounds_auck = [165, -52, 169, -49]
-    bounds_camp = [168, -53, 171, -51]
+    bounds_ant = [178, -50.5, 179.5, -47]
+    bounds_auck = [165.25, -51.5, 167.25, -49.5]
+    bounds_camp = [168.25, -53, 169.75, -52]
 
     whales_interp.geometry = whales_interp.geometry.apply(fix_dateline)
     vessel_points.geometry = vessel_points.geometry.apply(fix_dateline)
@@ -48,12 +62,10 @@ if __name__ == '__main__':
 
     # Generate frames
     os.makedirs('frames', exist_ok=True)
-    for i, ts in enumerate(timestamps):
-        print(i)
-        fig = heatmap.animation_frame(whales_interp[whale_mask], vessel_points[vessel_mask],
-                                      protected_areas, basemap, bounds_full, ts)
-        export_png(fig, filename=f'frames/frame_full_{i:04d}.png')
-
-        for label, bds in zip(['anti', 'auck', 'camp'], [bounds_ant, bounds_auck, bounds_camp]):
-            zoom_to_bounds(fig, bds)
-            export_png(fig, filename=f'frames/frame_{label}_{i:04d}.png')
+    for ts_label, ts_range in timestamps.items():
+        for i, ts in enumerate(ts_range):
+            for bds_label, bds in zip(['full', 'anti', 'auck', 'camp'], [bounds_full, bounds_ant, bounds_auck, bounds_camp]):
+                print(ts_label, bds_label, i)
+                fig = heatmap.animation_frame(whales_interp[whale_mask[ts_label]], vessel_points[vessel_mask[ts_label]],
+                                              protected_areas, basemap, bds, ts)
+                export_png(fig, filename=f'frames/frame_{ts_label}_{bds_label}_{i:04d}.png')
