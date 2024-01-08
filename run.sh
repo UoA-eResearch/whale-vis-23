23fa2b87c1cd974a57609102724bbed71756e1a2
Nelis Drost <nelis.drost@gmail.com>
2024-01-09 10:25:09 +1300

    Add extra runs to complete all frames

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 # Re-run the gen_anim_frames script multiple times, as it gets slower over time
 echo "$1"
-for i in `seq 1 50`
+for i in `seq 1 100`
 do
     # run the command multiple times (to refresh memory)
     python -m gen_anim_frames $1
